79dca30513f01108f60a4998df4f27aff0734ff6
Edwin Lee <leeed2001@gmail.com>
2022-10-04 16:56:11 -0500

    Update add_to_project.sh

--- add_to_project.sh
+++ add_to_project.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# call with the PR number as a command line argument
-# the PROJ_ID environment variable should be set from an ACTION SECRET
+# call with the PR number and the project id as 2 command line arguments
+
+PR_NUM=$1
+PROJ_ID=$2
+echo "Using PR Num as ${PR_NUM} and project ID as: ${PROJ_ID}"
 
 CONTENT=`gh pr view $1 --json 'id' --jq '.id'`
 echo "Found PR node ID as: ${CONTENT}"
-echo "Using Project ID as: ${PROJ_ID}"
-#
+
 # Need to get Node ID for the issue or PR to add if we do it this way
 gh api graphql -f query="
   mutation {
